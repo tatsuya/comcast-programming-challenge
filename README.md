4fd5cfaea6df650b343f08cf02165eb6ce451521
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-10-28 17:22:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# comcast-programming-challenge
+
+Solutions to [The Comcast MIT Career Fair Programming Challenge](https://www.linkedin.com/pulse/article/20140920001500-7852436-the-comcast-mit-career-fair-programming-challenge).
---
+++ main.c
@@ -0,0 +1,13 @@
+// Modify this code so that it prints
+// 20 minus signs. Add or substitute
+// 1 character only! There are 3 solutions.
+
+#include "stdio.h"
+
+int main() {
+  int i;
+  int n = 20;
+  for (i = 0; i < n; i--)
+    printf("-");
+  return 0;
+}
